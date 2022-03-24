92ae89b92d6302dc7ae7c268552c84aea0195b56
Muhammad Ali Mughal <jawantechpk@gmail.com>
2022-03-24 21:39:34 +0500

    class 1

---
+++ app.js
@@ -0,0 +1,169 @@
+// var a = " ";
+
+// if (a) {
+//   console.log("If Works");
+// }
+
+// 0 false
+// "" false
+// -1 true
+// [] true
+// {} true
+// null false
+// undefined false
+// true true
+// false false
+
+// var b;
+// function a() {
+//   b = "xyz";
+// }
+// a();
+// console.log(b);
+
+// if (true) {
+//   var a = "xyz";
+// }
+
+// console.log(a);
+
+// a();
+// function a() {
+//   console.log("abc");
+// }
+
+// var a = [];
+// a.abc = "asd"; // wrong
+// a[0] = "abc"; // right
+
+// var b = {};
+// b[0] = 'xyz' // wrong
+// b['pro1'] = 'xyz' // right
+
+// var a = "";
+// var check = Array.isArray(a);
+// console.log(check);
+
+// function a() {
+//   return function () {
+//     console.log("function B");
+//   };
+// }
+
+// a()();
+
+// var a = [];
+// a[0] = "asbd";
+// console.log(a);
+
+// var b = {};
+// b.p1 = "xyz";
+// b["p2"] = "jalksjd";
+// console.log(b);
+
+// var, let, const
+
+// var
+// block scope = false
+// re declareable = true
+// re assible = true
+// hoisting = true
+
+
+
+
+// let
+// block scope = true
+// redeclareable = false
+// re assible = true
+// hoisting = false
+
+
+// const
+// block scope = true
+// redeclareable = false
+// re assible = false
+// hoisting = false
+
+
+
+
+
+// if (true) {
+//   var a = "abc";
+// }
+
+// if (true) {
+//   let a = "xyz";
+// }
+
+// if (true) {
+//   const a = "xyz";
+// }
+
+// var a = 'abc'
+// var a = 123
+
+// let a = 'abc'
+// let a = 123
+
+// const a = 'abc'
+// const a = 123
+
+
+
+// var a = 'abc'
+// a = 123
+
+// let a = 'abc'
+// a = 123
+
+// const a = 'abc'
+// a = 123
+
+
+// hoisting
+// function, var
+
+
+// a()
+// function a(){
+//     console.log('abc')
+// }
+
+// if(true){
+//     var a = 'abc'
+// }
+
+
+// function a(){
+//     let b = 'abc'
+// }
+
+
+// b = 123
+// console.log(b);
+// var b = 'abc'
+
+// const a = 'abc'
+// a = 'xyz' 
+
+
+// const a = []
+// a = [1203]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
