af85ffc3971ff1badddb3acf42b5131d866826d0
BasitAyaz <basitahmed1997@gmail.com>
2022-05-13 21:44:36 +0500

    class 6 Material UI and Bootstrap

--- React Class 1/firstapp/src/card.js
+++ React Class 1/firstapp/src/card.js
@@ -1,12 +1,24 @@
-import { Button } from "./button";
+import Button from "@mui/material/Button";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 function Card(props) {
   return (
     <>
-      <div className="card">
-        <h3>{props.title}</h3>
-        <h2>{props.price}</h2>
-        <button onClick={props.action}>Add To Cart</button>
+      <div className="col-md-3 p-2">
+        <div className="border rounded shadow p-2">
+          <h3>{props.title}</h3>
+          <h2>{props.price}</h2>
+          <Button
+            onClick={props.action}
+            disabled={false}
+            variant="contained"
+            endIcon={<CameraAltIcon />}
+            fullWidth={true}
+            sx={{ backgroundColor: "purple", borderRadius: "20px" }}
+          >
+            Add To Cart
+          </Button>
+        </div>
       </div>
     </>
   );
